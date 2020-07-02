6d8a8b7d1fc58c128fdc9989289d2a4584acc9e2
suhyun <suhyun@172.17.218.21>
2020-07-02 16:22:20 +0900

    Created work frame;

---
+++ src/main/java/handong/edu/csee/s/WorkFrame.java
@@ -0,0 +1,13 @@
+package handong.edu.csee.s;
+
+import javax.swing.JComponent;
+
+public class WorkFrame extends JComponent {
+
+	
+	
+	public WorkFrame() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
